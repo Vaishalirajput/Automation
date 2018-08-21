b13a37a41f0209aa2fe1a5d39784c8b651db3d10
Vaishalirajput <rajput.vaishali19@gmail.com>
2018-08-21 17:36:41 +0530

    testing

--- src/credentials/LoginCredentials.java
+++ src/credentials/LoginCredentials.java
@@ -4,6 +4,7 @@ public class LoginCredentials {
 	public static final String VALID_USERNAME = "xx";
     public static final String VALID_PASSWORD = "xx";
     public static final String VALID_PASSWORDD = "xx";
+    public static final String VALID_PASSWORDDD = "xx";
 	 
 	   
 }
